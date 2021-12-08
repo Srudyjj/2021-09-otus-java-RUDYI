4fcbf70a51196fa6dc88da7ec78b452fc9bbf841
Serhii Rudyi <srudyjj@gmail.com>
2021-12-08 12:05:50 +0200

    hw06: Add withdraw

--- otus-java/hw06-atm/src/main/java/ru/otus/atm/CurrencyCell.java
+++ otus-java/hw06-atm/src/main/java/ru/otus/atm/CurrencyCell.java
@@ -1,16 +1,26 @@
 package ru.otus.atm;
 
+import java.util.LinkedList;
+
 public class CurrencyCell {
 
     private final Nominal nominal;
 
-    private int quantity;
+    private final LinkedList<Banknote> list = new LinkedList<>();
 
     public CurrencyCell(Nominal nominal) {
         this.nominal = nominal;
     }
 
-    public void add(int value) {
-        quantity += value;
+    public void add(Banknote banknote) {
+        list.add(banknote);
+    }
+
+    public Banknote remove() {
+        return list.removeLast();
+    }
+
+    public int size() {
+        return list.size();
     }
 }
