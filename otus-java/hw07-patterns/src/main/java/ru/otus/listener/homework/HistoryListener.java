1af0686627d52d52400f35a173f4f5f839b6ea1d
Serhii Rudyi <srudyjj@gmail.com>
2022-01-05 00:06:39 +0200

    hw07: Implement HistoryListener

--- otus-java/hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ otus-java/hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -2,17 +2,28 @@ package ru.otus.listener.homework;
 
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class HistoryListener implements Listener, HistoryReader {
+    
+    private final Map<Long, Message> history = new HashMap<>();
 
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        var field13 = new ObjectForMessage();
+        field13.setData(new ArrayList<>(msg.getField13().getData()));
+
+        Message messageCopy = msg.toBuilder().field13(field13).build();
+        history.put(messageCopy.getId(), messageCopy);
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return Optional.ofNullable(history.get(id));
     }
 }
